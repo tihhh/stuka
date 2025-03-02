9ede2eb12519808f957e95ba5ee74da641423545
Arzy <fatih.arrazy@gmail.com>
2025-03-03 02:38:00 +1100

    Added database and signing up

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import environ
+from os import environ, path
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -21,4 +21,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -28,7 +31,12 @@ def sign_up():
             flash('Password must be at least 7 characters.', category='error')
         else:
             #add user to database
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created successfully!', category='success')
-            pass
+            return redirect(url_for('views.home'))
+
+            
     return render_template("signup.html")
 
